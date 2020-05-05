3c548f050c512eb781fbbde57afca19a455f94ae
vlad_veko <veko.2001@gmail.com>
2020-05-05 22:24:53 +0300

    Add blacklist of domains

--- proxy.py
+++ proxy.py
@@ -3,6 +3,10 @@ import threading
 import signal
 import sys
 
+BLACKLIST_DOMAINS = ["facebook.com", "yandex.ru", "baskino.me"]
+
+# Function, that gets url and returns a host name of server
+# and a port, if there's one
 def parse_url(url):
     # Find position of ://
     pos = url.find("://")
@@ -34,13 +38,42 @@ def parse_url(url):
 
     return (webserver, port)
 
+def send_blacklist_responce(url, conn):
+    response_body = f"<html><body><h1>Proxy server blocked connection to {url}</h1></body></html>"
+
+    response_headers = {
+            'Content-Type': 'text/html; encoding=utf8',
+            'Content-Length': len(response_body),
+            'Connection': 'close',
+        }
+
+    # Reply as HTTP/1.1 server
+    response_proto = 'HTTP/1.1'
+    response_code = '403'
+    response_status_text = 'FORBIDDEN'
+
+    response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in response_headers.items())
+    
+    response_status = '%s %s %s' % (response_proto, response_code, response_status_text)
+
+    conn.send(response_status.encode('utf-8'))
+    conn.send(response_headers_raw.encode('utf-8'))
+    conn.send('\n'.encode('utf-8')) # to separate headers from body
+    conn.send(response_body.encode('utf-8'))
+
+    print(f"Url: {url} Code: 403")
+
+    # and closing connection, as we stated before
+    conn.close()
+
+# Function that modify the path in GET request
 def alter_request(request):
     # Parse the request into lines and get first line
     line = request.split("\n")[0]
     # Parse the first line into words
     words = line.split(" ")
 
-    # If it's not GET request, quit
+    # If it's not GET request, then quit
     if words[0] != "GET":
         return request
 
@@ -63,6 +96,7 @@ def alter_request(request):
 
     return request.replace(url, abs_path, 1)
 
+# Function, that returns HTTP responce code
 def getresponsecode(response):
     status_line = response.decode('utf-8', 'ignore').split("\n")[0]
     
@@ -86,16 +120,16 @@ class Server:
         self.server_socket.bind((host,port))
         
         # Now, listening for new connections  
-        self.server_socket.listen(10)
+        self.server_socket.listen(50)
 
     def listenForConn(self):
 
         while True:
             # Establish the connection
-            (client_socket, client_addr) = self.server_socket.accept() 
+            (connet, client_addr) = self.server_socket.accept() 
 
             # Create new thread
-            thread = threading.Thread(target=self.proxy, args=(client_socket, client_addr))
+            thread = threading.Thread(target=self.proxy, args=(connet, client_addr))
             thread.setDaemon(True)
             thread.start()
         self.shutdown()
@@ -106,19 +140,26 @@ class Server:
         request = conn.recv(8192)
         #print(request.decode('utf-8'))
 
-        # Parse the first line 
-        line = request.decode('utf-8', 'ignore').split('\n')[0]
-        # And get url 
-        url = line.split(' ')[1]
-        if not url:
-            sys.exit(0)
-        #print(url)
-
-        # Parse url to get webserver and port
-        (webserver, port) = parse_url(url)
-
-        request = alter_request(request.decode('utf-8')).encode('utf-8')
         try:
+            # Parse the first line 
+            line = request.decode('utf-8', 'ignore').split('\n')[0]
+            # And get url 
+            url = line.split(' ')[1]
+            if not url:
+                sys.exit(0)
+            #print(url)
+
+            # Check if the host:port is blacklisted
+            for i in range(0, len(BLACKLIST_DOMAINS)):
+                if BLACKLIST_DOMAINS[i] in url:
+                    send_blacklist_responce(url, conn)
+                    return
+            
+            # Parse url to get webserver and port
+            (webserver, port) = parse_url(url)
+
+            request = alter_request(request.decode('utf-8')).encode('utf-8')
+        
             # Send recieved request to the server
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(7)
@@ -153,7 +194,7 @@ class Server:
 
         except socket.error as error_msg:
             # If any error occurred, print error message
-            print("Error: ", addr, error_msg)
+            # print("Error: ", addr)
             
             # Close all resources
             if s:
@@ -161,6 +202,15 @@ class Server:
 
             if conn:
                 conn.close()
+
+        except IndexError:
+            # If any error occurred, print error message
+            # print("Error: ", addr)
+            
+            # Close all resources
+            if conn:
+                conn.close()
+
 
     def shutdown(self):
         self.server_socket.close()
